f35df0009f7a5fadb052da916be8719c9278b679
Kamernoa <kamernoa@gmail.com>
2022-02-09 17:15:47 +0200

    added premium limitation

--- playlist/__init__.py
+++ playlist/__init__.py
@@ -3,9 +3,16 @@ from typing import List
 
 
 class Playlist:
-    def __init__(self, name: str):
+    def __init__(self, name: str, premium=None):
         self.playlist_name: str = name
         self.playlist: List[Song] = []
+        if premium is None:
+            self.is_premium = False
+        else:
+            self.is_premium = premium
 
     def add_song(self, song: Song):
+        if not self.is_premium and len(self.playlist) == 20:
+            # throw exception "you cant add another playlist"
+            pass
         self.playlist.append(song)
--- user/__init__.py
+++ user/__init__.py
@@ -3,12 +3,19 @@ from typing import List
 
 
 class User:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str, premium=None):
         self.username = username
         self.password = password
+        if premium is None:
+            self.is_premium = False
+        else:
+            self.is_premium = premium
         self.playlists: List[Playlist] = []
 
     def add_playlist(self, name: str):
+        if not self.is_premium and len(self.playlists) == 5:
+            # throw exception "you cant add another playlist"
+            pass
         for playlist in self.playlists:
             if playlist.playlist_name == name:
                 # should throw exception "already have this playlist name"
