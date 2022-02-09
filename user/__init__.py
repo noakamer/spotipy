576c6f7bd1ced0b212f942e882d1df09c9de8c25
Kamernoa <kamernoa@gmail.com>
2022-02-09 14:54:39 +0200

    added 2 functions: add playlist and get playlist by name

--- user/__init__.py
+++ user/__init__.py
@@ -1,5 +1,22 @@
+from playlist import Playlist
+from typing import List
+
+
 class User:
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
-        self.playlist = []
+        self.playlists: List[Playlist] = []
+
+    def add_playlist(self, name: str):
+        for playlist in self.playlists:
+            if playlist.playlist_name == name:
+                # should throw exception "already have this playlist name"
+                pass
+        self.playlists.append(Playlist(name))
+
+    def get_playlist(self, name: str):
+        for playlist in self.playlists:
+            if playlist.playlist_name == name:
+                return playlist
+        # should throw exception "playlist_not_exist"
