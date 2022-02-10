b75b5a0d460a51037219d91b5dd3deafc63dadc1
Kamernoa <kamernoa@gmail.com>
2022-02-10 14:30:42 +0200

    tried to use flusk, didn't suceed

--- application/__init__.py
+++ application/__init__.py
@@ -1,20 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, redirect, url_for, request
+import jinja2
 import const
 
 app = Flask(__name__)
 
 
-@app.route("/hi", methods=["GET"])
-def hello():
-    return "hello world"
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
 
 
-@app.route("/bye", methods=["GET"])
-def bye():
-    return "bye world"
+@app.route('/welcome')
+def welcome():
+    return render_template('application/welcome.html')  # render a template
 
 
-@app.route("/artists", methods=["GET"])
-def artists():
-    return const.LIST_OF_ARTISTS
-
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
